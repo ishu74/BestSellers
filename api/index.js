03b47d501077072bb1da45bd4e03d1061f9cc451
Aayush <125236907+ishu74@users.noreply.github.com>
2024-02-25 00:54:50 +0530

    first commit

--- api/index.js
+++ api/index.js
@@ -4,13 +4,15 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
-import listingRouter from "../api/routes/listing.route.js"
+import listingRouter from "../api/routes/listing.route.js";
+import path from "path";
+
 
 //configuration
 dotenv.config();
 const app = express();
 app.use(express.json()); // allow server to json data
-app.use(cookieParser())
+app.use(cookieParser());
 
 mongoose
   .connect(process.env.MONGO)
@@ -21,6 +23,14 @@ mongoose
     console.log("MongoDB connection Error", err);
   });
 
+
+  //deployment
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
 app.listen(3000, () => {
   console.log("Server is runnig on PORT 3000!!!!");
 });
@@ -28,14 +38,14 @@ app.listen(3000, () => {
 //api routing authentication
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
-app.use("/api/listing", listingRouter)
+app.use("/api/listing", listingRouter);
+
 
 //middle ware for handlinf error
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
-  return res.status(statusCode).
-  json({
+  return res.status(statusCode).json({
     success: false,
     message,
     statusCode,
